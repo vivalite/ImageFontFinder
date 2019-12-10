0acdba8ba2c23a3e258d8ec727f1c7f77530ec3b
Wei Shi <vivalite@gmail.com>
2019-12-10 00:54:26 -0500

    Testing basic samples successful

--- ImageFontFinder/Main.cs
+++ ImageFontFinder/Main.cs
@@ -22,7 +22,8 @@ namespace ImageFontFinder
 {
     public partial class Main : Form
     {
-        Baidu.Aip.Ocr.Ocr client;
+        Baidu.Aip.Ocr.Ocr _client;
+        List<TextSegmentData> _textSegments = new List<TextSegmentData>();
 
         public Main()
         {
@@ -32,8 +33,10 @@ namespace ImageFontFinder
             var API_KEY = "34cIesqLIgj2advlwktx0D0K";
             var SECRET_KEY = "GUmlGTeFtIfqctLWLRabiV2aWrliPltB";
 
-            client = new Baidu.Aip.Ocr.Ocr(API_KEY, SECRET_KEY);
-            client.Timeout = 60000;
+            _client = new Baidu.Aip.Ocr.Ocr(API_KEY, SECRET_KEY)
+            {
+                Timeout = 60000
+            };
 
         }
 
@@ -44,6 +47,8 @@ namespace ImageFontFinder
 
         private void LoadImage()
         {
+            _textSegments.Clear();
+
             List<Mat> croppedTexts = new List<Mat>();
             byte[] image = File.ReadAllBytes(@".\test.jpg");
 
@@ -59,7 +64,7 @@ namespace ImageFontFinder
                 {"detect_language", "true"}
             };
 
-            JObject resultJson = client.Accurate(image, options);
+            JObject resultJson = _client.Accurate(image, options);
             //Debug.Print(resultJson.ToString());
 
             dynamic ocrResult = JsonConvert.DeserializeObject<dynamic>(resultJson.ToString());
@@ -68,44 +73,53 @@ namespace ImageFontFinder
 
             for (int i = 0; i < wordCount; i++)
             {
-                string blobText = ocrResult.words_result[i].words;
                 dynamic chars = ocrResult.words_result[i].chars;
 
                 for (int j = 0; j < chars.Count; j++)
                 {
-                    string theChar = chars[j]["char"];
-                    int width = chars[j].location.width;
-                    int top = chars[j].location.top;
-                    int left = chars[j].location.left;
-                    int height = chars[j].location.height;
-
-                    Debug.Print($"Text: {theChar}, IsCJK? {theChar.Any(x => x.IsChinese())}, W {width}, H {height}, T {top}, L {left} ");
-
-                    if (theChar.Any(x => x.IsChinese()))
+                    TextSegmentData segmentData = new TextSegmentData
                     {
-                        Rect cropTextRect = new Rect(left, top, (int)(width * 1.8), (int)(height * 1.2));
+                        TextLine = ocrResult.words_result[i].words,
+                        TextLineWidth = ocrResult.words_result[i].location.width,
+                        TextLineTop = ocrResult.words_result[i].location.top,
+                        TextLineLeft = ocrResult.words_result[i].location.left,
+                        TextLineHeight = ocrResult.words_result[i].location.height,
+                        TextChar = chars[j]["char"],
+                        TextCharWidth = chars[j].location.width,
+                        TextCharTop = chars[j].location.top,
+                        TextCharLeft = chars[j].location.left,
+                        TextCharHeight = chars[j].location.height
+                    };
+                    segmentData.IsCJK = segmentData.TextChar.Any(x => x.IsChinese());
+
+                    Debug.Print($"Text: {segmentData.TextChar}, IsCJK? {segmentData.IsCJK}, W {segmentData.TextCharWidth}, H {segmentData.TextCharHeight}, T {segmentData.TextCharTop}, L {segmentData.TextCharLeft} ");
+
+                    if (segmentData.IsCJK)
+                    {
+                        Rect cropTextRect = new Rect(segmentData.TextCharLeft, segmentData.TextCharTop, (int)(segmentData.TextCharWidth * 2), (int)(segmentData.TextCharHeight * 1.2));
                         displayMat.Rectangle(cropTextRect, Scalar.RandomColor(), 2, LineTypes.AntiAlias);
 
                         Mat croppedText = new Mat(originalMat, cropTextRect);
                         croppedTexts.Add(croppedText);
+                        segmentData.TextCharCroppedMat = croppedText.Clone();
 
                         //croppedText.SaveImage("!" + Guid.NewGuid() + ".png");
                     }
+
+                    _textSegments.Add(segmentData);
                 }
             }
 
-            int widthTarget = 80;
-            int heightTarget = 80;
-
+            int netInputWidth = 80;
+            int netInputHeight = 80;
 
             using (Net net = CvDnn.ReadNetFromTensorflow("all_freezed_vgg19_tf18.pb"))
             {
-
-                foreach (Mat croppedText in croppedTexts)
+                foreach (TextSegmentData sgData in _textSegments.Where(x => x.IsCJK).ToArray())
                 {
                     // preprocess
 
-                    Mat greyText = croppedText.CvtColor(ColorConversionCodes.BGR2GRAY);
+                    Mat greyText = sgData.TextCharCroppedMat.CvtColor(ColorConversionCodes.BGR2GRAY);
 
                     Mat textAfterGradMorph = new Mat();
                     Mat kernel = Cv2.GetStructuringElement(MorphShapes.Ellipse, new Size(2, 2));
@@ -114,69 +128,133 @@ namespace ImageFontFinder
                     Mat textAfterThreshold = new Mat();
                     Cv2.Threshold(greyText, textAfterThreshold, 0, 255, ThresholdTypes.Binary | ThresholdTypes.Otsu);
 
+                    Mat textAfterMorph = new Mat();
                     kernel = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(2, 2));
-                    Cv2.MorphologyEx(textAfterThreshold, textAfterThreshold, MorphTypes.Open, kernel);
-                    Cv2.MorphologyEx(textAfterThreshold, textAfterThreshold, MorphTypes.Close, kernel);
-
-
-
-
-                    //Mat textDistance = new Mat(croppedText.Size(), MatType.CV_32F);
-                    //Cv2.DistanceTransform(textAfterThreshold, textDistance, DistanceTypes.L2, DistanceMaskSize.Precise);
-
-                    //Cv2.Normalize(textDistance,textDistance);
-
-                    //Mat textDistanceBw32F = new Mat(croppedText.Size(), MatType.CV_32F);
-                    //double stthreshold = 2.0;
-                    //Cv2.Threshold(textDistance, textDistanceBw32F, 0.1, 1.0, ThresholdTypes.Binary);
-
-                    //Mat textDistanceBw8U = new Mat(croppedText.Size(), MatType.CV_8UC1);
-                    //textDistanceBw32F.ConvertTo(textDistanceBw8U, MatType.CV_8UC1);
-
+                    Cv2.MorphologyEx(textAfterThreshold, textAfterMorph, MorphTypes.Open, kernel);
+                    Cv2.MorphologyEx(textAfterMorph, textAfterMorph, MorphTypes.Close, kernel);
 
                     // resize
-                    double scaleW = widthTarget / (double)croppedText.Width;
-                    double scaleH = heightTarget / (double)croppedText.Height;
+
+                    double scaleW = netInputWidth / (double)sgData.TextCharCroppedMat.Width;
+                    double scaleH = netInputHeight / (double)sgData.TextCharCroppedMat.Height;
                     double scale = scaleW < scaleH ? scaleW : scaleH;
 
                     Mat resizedText = new Mat();
-                    Cv2.Resize(textAfterThreshold, resizedText, new OpenCvSharp.Size(0, 0), scale, scale,
-                        InterpolationFlags.Cubic);
+                    Cv2.Resize(textAfterMorph, resizedText, new Size(0, 0), scale, scale, InterpolationFlags.Cubic);
 
                     resizedText = resizedText.CvtColor(ColorConversionCodes.GRAY2BGR);
 
                     //Cv2.ImShow("" + Guid.NewGuid(), resizedText);
 
-                    //continue;
-
                     int classId1;
                     double classProb1;
 
-
-
-                    var inputBlob = CvDnn.BlobFromImage(resizedText, 1, new Size(widthTarget, heightTarget));
+                    var inputBlob = CvDnn.BlobFromImage(resizedText, 1, new Size(netInputWidth, netInputHeight));
                     net.SetInput(inputBlob);
                     var prob = net.Forward();
                     GetMaxClass(prob, out classId1, out classProb1);
+                    sgData.ClassLable1 = GetClassText(classId1);
+                    sgData.ClassProb1 = classProb1;
 
                     int classId2;
                     double classProb2;
 
                     Cv2.BitwiseNot(resizedText, resizedText);
 
-                    inputBlob = CvDnn.BlobFromImage(resizedText, 1, new Size(widthTarget, heightTarget));
+                    inputBlob = CvDnn.BlobFromImage(resizedText, 1, new Size(netInputWidth, netInputHeight));
                     net.SetInput(inputBlob);
                     prob = net.Forward();
                     GetMaxClass(prob, out classId2, out classProb2);
+                    sgData.ClassLable2 = GetClassText(classId2);
+                    sgData.ClassProb2 = classProb2;
 
-                    Debug.Print(
-                        $"ClassID:{GetClassText(classId1)}, classProb:{classProb1} ClassID2:{GetClassText(classId2)}, classProb2:{classProb2}");
+                    Debug.Print($"ClassID:{GetClassText(classId1)}, classProb:{classProb1} ClassID2:{GetClassText(classId2)}, classProb2:{classProb2}");
                 }
             }
 
-
+            // done processing, output
 
             pictureBoxOriginal.Image = displayMat.ToBitmap();
+
+            var groupedTextLines = _textSegments.Where(x => x.IsCJK).GroupBy(
+                x => new
+                {
+                    x.TextLineWidth,
+                    x.TextLineTop,
+                    x.TextLineLeft,
+                    x.TextLineHeight
+                }).ToArray();
+
+
+            foreach (var textLine in groupedTextLines)
+            {
+                Dictionary<string, int> fontListFreq = new Dictionary<string, int>();
+
+                foreach (TextSegmentData data in textLine)
+                {
+                    if (!fontListFreq.ContainsKey(data.ClassLable1))
+                    {
+                        fontListFreq.Add(data.ClassLable1, 1);
+                    }
+                    else
+                    {
+                        fontListFreq[data.ClassLable1]++;
+                    }
+
+                    if (!fontListFreq.ContainsKey(data.ClassLable2))
+                    {
+                        fontListFreq.Add(data.ClassLable2, 1);
+                    }
+                    else
+                    {
+                        fontListFreq[data.ClassLable2]++;
+                    }
+
+                }
+
+                var orderedFontOccurenceList = fontListFreq.OrderByDescending(x => x.Value).ToArray();
+
+                if (orderedFontOccurenceList.Length > 1)
+                {
+                    if (orderedFontOccurenceList[0].Value > orderedFontOccurenceList[1].Value)
+                    {
+                        // significant font found
+                        Debug.Print($"Text Line: {textLine.FirstOrDefault()?.TextLine}, Font Name: {orderedFontOccurenceList[0].Key}");
+                    }
+                    else
+                    {
+                        // no significant font found, take the highest probability
+                        Dictionary<string, double> fontListProb = new Dictionary<string, double>();
+
+                        foreach (TextSegmentData data in textLine)
+                        {
+                            if (!fontListProb.ContainsKey(data.ClassLable1))
+                            {
+                                fontListProb.Add(data.ClassLable1, data.ClassProb1);
+                            }
+                            else
+                            {
+                                fontListProb[data.ClassLable1] += data.ClassProb1;
+                            }
+
+                            if (!fontListProb.ContainsKey(data.ClassLable2))
+                            {
+                                fontListProb.Add(data.ClassLable2, data.ClassProb2);
+                            }
+                            else
+                            {
+                                fontListProb[data.ClassLable2] += data.ClassProb2;
+                            }
+                        }
+
+                        var orderedFontProbList = fontListProb.OrderByDescending(x => x.Value).ToArray();
+
+                        Debug.Print($"Text Line: {textLine.FirstOrDefault()?.TextLine}, Font Name: {orderedFontProbList[0].Key}");
+
+                    }
+                }
+
+            }
         }
 
         Dictionary<int, string> classLable = new Dictionary<int, string>();
@@ -210,5 +288,28 @@ namespace ImageFontFinder
             Cv2.MinMaxLoc(probMat, out _, out classProb, out _, out var classNumber);
             classId = classNumber.X;
         }
+
+
+    }
+
+    public class TextSegmentData
+    {
+        public string TextLine { get; set; }
+        public int TextLineTop { get; set; }
+        public int TextLineLeft { get; set; }
+        public int TextLineWidth { get; set; }
+        public int TextLineHeight { get; set; }
+        public string TextChar { get; set; }
+        public int TextCharTop { get; set; }
+        public int TextCharLeft { get; set; }
+        public int TextCharWidth { get; set; }
+        public int TextCharHeight { get; set; }
+        public bool IsCJK { get; set; }
+        public Mat TextCharCroppedMat { get; set; }
+        public string ClassLable1 { get; set; }
+        public double ClassProb1 { get; set; }
+        public string ClassLable2 { get; set; }
+        public double ClassProb2 { get; set; }
+
     }
 }
