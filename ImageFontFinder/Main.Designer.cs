3f5eecc18723e246e08624680cd0c0ea9c95b2da
Wei Shi <vivalite@gmail.com>
2019-12-30 00:27:36 -0500

    update to 90% accuracy model

--- ImageFontFinder/Main.Designer.cs
+++ ImageFontFinder/Main.Designer.cs
@@ -33,8 +33,6 @@
             this.tableLayoutPanelResult = new System.Windows.Forms.TableLayoutPanel();
             this.pictureBoxGenerated = new System.Windows.Forms.PictureBox();
             this.pictureBoxOriginalCrop = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.labelFontInfo = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.buttonTest = new System.Windows.Forms.Button();
@@ -78,14 +76,12 @@
             // 
             // tableLayoutPanelResult
             // 
-            this.tableLayoutPanelResult.ColumnCount = 2;
-            this.tableLayoutPanelResult.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanelResult.ColumnCount = 1;
             this.tableLayoutPanelResult.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanelResult.Controls.Add(this.pictureBoxGenerated, 1, 1);
-            this.tableLayoutPanelResult.Controls.Add(this.pictureBoxOriginalCrop, 1, 0);
-            this.tableLayoutPanelResult.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanelResult.Controls.Add(this.label2, 0, 1);
-            this.tableLayoutPanelResult.Controls.Add(this.labelFontInfo, 1, 2);
+            this.tableLayoutPanelResult.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelResult.Controls.Add(this.pictureBoxGenerated, 0, 1);
+            this.tableLayoutPanelResult.Controls.Add(this.pictureBoxOriginalCrop, 0, 0);
+            this.tableLayoutPanelResult.Controls.Add(this.labelFontInfo, 0, 2);
             this.tableLayoutPanelResult.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanelResult.Location = new System.Drawing.Point(3, 427);
             this.tableLayoutPanelResult.Name = "tableLayoutPanelResult";
@@ -99,9 +95,9 @@
             // pictureBoxGenerated
             // 
             this.pictureBoxGenerated.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxGenerated.Location = new System.Drawing.Point(103, 80);
+            this.pictureBoxGenerated.Location = new System.Drawing.Point(3, 80);
             this.pictureBoxGenerated.Name = "pictureBoxGenerated";
-            this.pictureBoxGenerated.Size = new System.Drawing.Size(801, 71);
+            this.pictureBoxGenerated.Size = new System.Drawing.Size(901, 71);
             this.pictureBoxGenerated.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxGenerated.TabIndex = 1;
             this.pictureBoxGenerated.TabStop = false;
@@ -109,41 +105,19 @@
             // pictureBoxOriginalCrop
             // 
             this.pictureBoxOriginalCrop.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxOriginalCrop.Location = new System.Drawing.Point(103, 3);
+            this.pictureBoxOriginalCrop.Location = new System.Drawing.Point(3, 3);
             this.pictureBoxOriginalCrop.Name = "pictureBoxOriginalCrop";
-            this.pictureBoxOriginalCrop.Size = new System.Drawing.Size(801, 71);
+            this.pictureBoxOriginalCrop.Size = new System.Drawing.Size(901, 71);
             this.pictureBoxOriginalCrop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxOriginalCrop.TabIndex = 0;
             this.pictureBoxOriginalCrop.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(19, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 18);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Original:";
-            // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(5, 106);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 18);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Infered Font:";
             // 
             // labelFontInfo
             // 
             this.labelFontInfo.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labelFontInfo.AutoSize = true;
             this.labelFontInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.labelFontInfo.Location = new System.Drawing.Point(503, 165);
+            this.labelFontInfo.Location = new System.Drawing.Point(453, 165);
             this.labelFontInfo.Name = "labelFontInfo";
             this.labelFontInfo.Size = new System.Drawing.Size(0, 18);
             this.labelFontInfo.TabIndex = 4;
@@ -169,6 +143,7 @@
             this.buttonTest.TabIndex = 5;
             this.buttonTest.Text = "Test";
             this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Visible = false;
             this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
             // 
             // buttonLoadImage
@@ -212,8 +187,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button buttonTest;
         private System.Windows.Forms.Button buttonLoadImage;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label labelFontInfo;
     }
 }
